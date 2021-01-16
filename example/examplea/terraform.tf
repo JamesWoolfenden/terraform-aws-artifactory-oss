af288492b22f75b406035d0a50950ba4ba611978
James Woolfenden <James.Woolfenden@gmail.com>
2021-01-16 22:54:30 +0000

    v 2021

---
+++ example/examplea/terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "3.24.1"
+      source  = "hashicorp/aws"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.0.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.0.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.0.0"
+    }
+  }
+  required_version = ">= 0.14"
+}
